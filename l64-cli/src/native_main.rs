f1f1f5d2e93e0cc0eaa432ccb0efaf539639fdb3
FreshSoftware4 <99464939+FreshSoftware4@users.noreply.github.com>
2026-07-22 22:59:09 -0700

    Demote legacy CLI contacts to migration ingress

--- l64-cli/src/native_main.rs
+++ l64-cli/src/native_main.rs
@@ -9,9 +9,20 @@ mod legacy {
 }
 
 fn main() -> anyhow::Result<()> {
+    if native_membrane::is_legacy_child() {
+        native_membrane::announce_explicit_legacy();
+        return legacy::run();
+    }
+    if let Some(code) = native_membrane::run_explicit_legacy().map_err(anyhow::Error::msg)? {
+        if code == 0 {
+            return Ok(());
+        }
+        std::process::exit(code);
+    }
     if native_membrane::run_env().map_err(anyhow::Error::msg)? {
         Ok(())
     } else {
+        native_membrane::warn_ambient_legacy_contact();
         legacy::run()
     }
 }
