d1850250452f2a555cf228900af4b0f92cd7c98a
FreshSoftware4 <99464939+FreshSoftware4@users.noreply.github.com>
2026-07-22 22:36:00 -0700

    Expose compact native RNA ingress

--- l64-native/src/lib.rs
+++ l64-native/src/lib.rs
@@ -6,6 +6,7 @@ mod dna;
 mod graph;
 mod journal;
 mod kernel;
+mod rna;
 mod route;
 
 pub use codec::{DecodeError, canonical_bytes, decode_canonical};
@@ -14,4 +15,7 @@ pub use dna::{DnaError, MAX_NATIVE_DNA_PAYLOAD_BYTES, decode_dna, dna_bytes};
 pub use graph::{ContextId, EventId, Graph, Node, NodeId, ROOT_CONTEXT};
 pub use journal::JournalEvent;
 pub use kernel::{CommitResult, Obstruction, OpCode, Port, PortRole, Proposal};
+pub use rna::{
+    MAX_NATIVE_RNA_BYTES, RnaError, compile_rna, dna_to_rna, normalize_rna, rna_bytes, rna_to_dna,
+};
 pub use route::{LocusWord, Route};
